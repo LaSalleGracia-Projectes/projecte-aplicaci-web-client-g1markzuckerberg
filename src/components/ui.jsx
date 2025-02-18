136399fde653925820f8e7509e0bc896c5c9aa04
RogerBustos <Rogerbj22@gmail.com>
2025-02-18 16:11:44 +0100

    header done

---
+++ src/components/ui.jsx
@@ -0,0 +1,3 @@
+'use client';
+import { Card, CardBody, Input, Textarea, Button, Typography, CardFooter, List, ListItem, IconButton, Drawer } from "@material-tailwind/react";
+export { Card, CardBody, Input, Textarea, Button, Typography, CardFooter, List, ListItem, IconButton, Drawer };
