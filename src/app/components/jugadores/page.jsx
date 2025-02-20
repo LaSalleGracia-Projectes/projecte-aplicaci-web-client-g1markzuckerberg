f2a461767ade196d57ff1b37846f696ce074085b
RogerBustos <Rogerbj22@gmail.com>
2025-02-20 18:55:14 +0100

    pitch for jornada

--- src/app/components/jugadores/page.jsx
+++ src/app/components/jugadores/page.jsx
@@ -1,5 +1,6 @@
 // pages/index.js
 import Layout from "@/components/layout";
+import Image from "next/image";
 
 export default function Jugadores() {
   return (
@@ -7,7 +8,6 @@ export default function Jugadores() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h1 className="text-3xl font-bold mb-4 text-gray-900">Bienvenido a Mi Aplicación</h1>
         <p className="text-lg text-gray-700">
-          Esta es la página de inicio de nuestra aplicación.
         </p>
       </div>
     </Layout>
