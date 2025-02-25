e23409d22de7021b312ed18c8521fd8fc2df4d2b
RogerBustos <Rogerbj22@gmail.com>
2025-02-25 15:36:02 +0100

    some pages dones

---
+++ src/components/clasificacion/columnas.jsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+const players = [
+  { id: 1, name: "Jugador 1", score: 95, image: "/images/player1.png" },
+  { id: 2, name: "Jugador 2", score: 88, image: "/images/player2.png" },
+  { id: 3, name: "Jugador 3", score: 76, image: "/images/player3.png" },
+];
+
+export default function PlayerInfoPage() {
+  return (
+    <div className="grid grid-cols-3 gap-6 p-6">
+      {/* Sección izquierda con imagen y descripción */}
+      <div className="col-span-1 flex flex-col items-center">
+        <Image src="/images/team.png" alt="Equipo" width={200} height={200} className="rounded-lg" />
+        <p className="mt-4 text-gray-600 text-center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula ligula eget augue.
+        </p>
+      </div>
+      
+      {/* Sección derecha con la información de los jugadores */}
+      <div className="col-span-2 flex flex-col space-y-4">
+        {players.map((player) => (
+          <div key={player.id} className="flex items-center bg-gray-100 p-4 rounded-lg shadow">
+            <Image src={player.image} alt={player.name} width={50} height={50} className="rounded-full" />
+            <div className="ml-4 flex-1">
+              <p className="text-lg font-semibold">{player.name}</p>
+            </div>
+            <p className="text-xl font-bold text-blue-500">{player.score}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
