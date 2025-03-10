9bb30c61b31978e0d7befd21f5cd7ef0e90b1cdc
RogerBustos <Rogerbj22@gmail.com>
2025-03-10 16:13:25 +0100

    popups from ajustes done

--- src/components/ui.jsx
+++ src/components/ui.jsx
@@ -1,3 +1,3 @@
 'use client';
-import { Card, CardBody, Input, Textarea, Button, Typography, CardFooter, List, ListItem, IconButton, Drawer, Select, Option } from "@material-tailwind/react";
-export { Card, CardBody, Input, Textarea, Button, Typography, CardFooter, List, ListItem, IconButton, Drawer, Select, Option };
+import { Card, CardBody, Input, Textarea, Button, Typography, CardFooter, List, ListItem, IconButton, Drawer, Select, Option, Label, Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@material-tailwind/react";
+export { Card, CardBody, Input, Textarea, Button, Typography, CardFooter, List, ListItem, IconButton, Drawer, Select, Option, Label, Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter };
